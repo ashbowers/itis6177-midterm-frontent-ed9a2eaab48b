d6b10f74831030e8a75473f4711bf7e128309003
Ash Bowers <abower16@uncc.edu>
2020-03-17 15:16:24 -0400

    initial commit

---
+++ build/precache-manifest.d88946dcdef6c528c0cbf88ee71d1283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcde56fa1b30c591900d9010c0a70a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb8e13340eb8db19744b",
+    "url": "./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "3939393d714e47de725a",
+    "url": "./static/css/main.e718cee9.chunk.css"
+  },
+  {
+    "revision": "fb8e13340eb8db19744b",
+    "url": "./static/js/2.bc01748a.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "./static/js/2.bc01748a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3939393d714e47de725a",
+    "url": "./static/js/main.c515845b.chunk.js"
+  },
+  {
+    "revision": "58d6219fe1f60f2fa5af",
+    "url": "./static/js/runtime-main.f1c188a0.js"
+  }
+]);
